0e814377d57a6530f5990a76e6c4b0229c9e7e6b
Weronika Żak <weronikarzak@gmail.com>
2020-01-05 02:37:00 +0100

    assistant simple answering

--- main.py
+++ main.py
@@ -10,4 +10,23 @@ def speak(text):
     tts.save(filename)
     playsound.playsound(filename)
 
-speak("hello there uwu")
+def get_audio():
+    r = sr.Recognizer()
+    with sr.Microphone() as soucre:
+        audio = r.listen(soucre)
+        said =""
+
+        try:
+            said = r.recognize_google(audio)
+            print(said)
+        except Exception as e:
+            print("Exception: " + str(e))
+    
+    return said
+
+text = get_audio()
+
+if "hello" in text:
+    speak("hello, how are you?")
+elif "what is your name" in text:
+    speak("My name is Ezra")
