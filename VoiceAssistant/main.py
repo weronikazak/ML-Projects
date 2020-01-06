077921d09ce7f132eb25ff24f37f04de95c52c39
Weronika Żak <weronikarzak@gmail.com>
2020-01-06 23:43:29 +0100

    add google-calendar to VA

--- VoiceAssistant/main.py
+++ VoiceAssistant/main.py
@@ -1,8 +1,17 @@
+from __future__ import print_function
+import os.path
+import pickle
 import speech_recognition as sr
 from gtts import gTTS
 import os
+import datetime
 import time
 import playsound
+from googleapiclient.discovery import build
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
 def speak(text):
     tts = gTTS(text=text, lang='en')
@@ -24,9 +33,42 @@ def get_audio():
     
     return said
 
-text = get_audio()
+def authenticate_google():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
 
-if "hello" in text:
-    speak("hello, how are you?")
-elif "what is your name" in text:
-    speak("My name is Ezra")
+    service = build('calendar', 'v3', credentials=creds)
+    return service
+
+def get_events(n, service):
+    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+    print(f'Getting the upcoming {n} events')
+    events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=n, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        print('No upcoming events found.')
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+
+service = authenticate_google()
+get_events(2, service)
