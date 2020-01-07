3b5933a09d3df475d400dbccfe050b87eda4d9c7
Weronika Żak <weronikarzak@gmail.com>
2020-01-07 23:47:57 +0100

    get spoken date

--- VoiceAssistant/main.py
+++ VoiceAssistant/main.py
@@ -7,17 +7,71 @@ import os
 import datetime
 import time
 import playsound
+import pyttsx3
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+MONTHS = ["january", "february", "march", "april", "may", "june", "july", "august", "september"
+            "october", "november", "december"]
+DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+DAY_EXTENTION = ['rd', 'th', 'st', 'nd']
+
+def get_date(text):
+    text = text.lower()
+    today = datetime.date.today()
+
+    if text.count('today') > 0:
+        return today
+
+    day = -1
+    day_of_week = -1
+    month = -1
+    year = today.year
+
+    for word in text.split():
+        if word in MONTHS:
+            month = MONTHS.index(word) + 1
+        elif word in DAYS:
+            day = DAYS.index(word)
+        elif word.isdigit():
+            day = int(word)
+        else:
+            for ext in DAY_EXTENTION:
+                found = word.find(ext)
+                if found > 0:
+                    try:
+                        day = int(word[:found])
+                    except:
+                        pass
+
+
+    if month < today.month and month != -1:
+        current_day_of_week = today.weekday()
+        dif = day_of_week - current_day_of_week
+
+        if dif < 0:
+            dif += 7
+            if text.count("next") >= 1:
+                dif += 7
+
+        return today + datetime.timedelta(dif)
+
+    if day != -1:
+        return datetime.date(month=month, day=day, year=year)
+
+
 
 def speak(text):
-    tts = gTTS(text=text, lang='en')
-    filename = 'voice.mp3'
-    tts.save(filename)
-    playsound.playsound(filename)
+    engine = pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+    # tts = gTTS(text=text, lang='en')
+    # filename = 'voice.mp3'
+    # tts.save(filename)
+    # playsound.playsound(filename)
+
 
 def get_audio():
     r = sr.Recognizer()
@@ -33,15 +87,13 @@ def get_audio():
     
     return said
 
+
 def authenticate_google():
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -49,18 +101,23 @@ def authenticate_google():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
     return service
 
-def get_events(n, service):
-    now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-    print(f'Getting the upcoming {n} events')
-    events_result = service.events().list(calendarId='primary', timeMin=now,
-                                        maxResults=n, singleEvents=True,
+
+def get_events(day, service):
+    # Call the Calendar API
+    date = datetime.datetime.combine(day, datetime.datetime.min.time())
+    end = datetime.datetime.combine(day, datetime.datetime.max.time())
+    utc = pytz.UTC
+    date = date.astimezone(utc)
+    end = end.astimezone(utc)
+    events_result = service.events().list(calendarId='primary', timeMin=date.isoformat(),
+                                        timeMax=end.isoformat(), singleEvents=True,
                                         orderBy='startTime').execute()
     events = events_result.get('items', [])
 
@@ -70,5 +127,7 @@ def get_events(n, service):
         start = event['start'].get('dateTime', event['start'].get('date'))
         print(start, event['summary'])
 
-service = authenticate_google()
-get_events(2, service)
+SERVICE = authenticate_google()
+# get_events(2, service)
+text = get_audio()
+get_events(get_date(text), SERVICE)
