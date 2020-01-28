d534afd8b49e068a138845eb05bbc71aace02ed6
Weronika Żak <weronikarzak@gmail.com>
2020-01-28 10:59:49 +0100

    segment and thteshold a hand

---
+++ GestureRecognizer/gesture.py
@@ -0,0 +1,72 @@
+import cv2
+import imutils
+import numpy as np
+
+bg = None
+
+def runninig_average(image, aWeight):
+    global bg
+
+    if bg is None:
+        bg = image.copy().astype("float")
+        return
+
+    cv2.accumulateWeighted(image, bg, aWeight)
+
+def segment(image, threshold=25):
+    global bg
+
+    diff = cv2.absdiff(bg.astype("uint8"), image)
+
+    thresholded = cv2.threshold(diff, threshold, 255, cv2.THRESH_BINARY)[1]
+
+    contours, hierarchy = cv2.findContours(thresholded.copy(),
+                        cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    
+    if len(contours) == 0:
+        return
+    else:
+        segmented = max(contours, key=cv2.contourArea)
+        return (thresholded, segmented)
+
+        
+
+aWeight = .5
+cap = cv2.VideoCapture(0)
+top, right, bottom, left = 10, 350, 225, 590
+num_frames = 0
+
+while True:
+    ret, frame = cap.read()
+    # cv2.imshow(frame, 'frame')
+    frame = imutils.resize(frame, width=700, height=700)
+    frame = cv2.flip(frame, 1)
+    clone = frame.copy()
+    (height, width) = frame.shape[:2]
+    roi = frame[top:bottom, right:left]
+
+    gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (7,7), 0)
+
+    if num_frames < 30:
+        runninig_average(gray, aWeight)
+    else:
+        hand = segment(gray)
+
+        if hand is not None:
+            (thresholded, segmented) = hand
+            cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+            cv2.imshow("Thresholded", thresholded)
+
+    cv2.rectangle(clone, (left, top), (right, bottom), (0, 255, 0), 2)
+
+    num_frames += 1
+
+    cv2.imshow("Video Feed", clone)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+camera.release()
+cv2.destroyAllWindows()
+
